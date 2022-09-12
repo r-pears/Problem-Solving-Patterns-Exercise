acb1a9e38cbefc3c36d031ac5c97fa47898f9c0c
Ryan <ryjen.pears@gmail.com>
2022-09-12 16:37:24 +0200

    construct note added

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,25 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(message, letters) {
+  const lettersFreq = {};
+  const messageFreq = {};
+
+  for (let char of letters) {
+    lettersFreq[char] = ++lettersFreq[char] || 1;
+  }
+
+  for (let char of message) {
+    messageFreq[char] = ++messageFreq[char] || 1;
+  }
+
+  for (let char in messageFreq) {
+    if (!lettersFreq[char]) {
+      return false;
+    }
+
+    if (messageFreq[char] > lettersFreq[char]) {
+      return false;
+    }
+  }  
+
+  return true;
 }
